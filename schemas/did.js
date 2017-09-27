fcc2e0694c0848f1cdd73e6e3f39f8da2d46839e
Matthew Collier <collier.matthew@gmail.com>
2017-09-27 10:26:56 -0400

    Implement schema validation.

---
+++ schemas/did.js
@@ -0,0 +1,24 @@
+/*!
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+const bedrock = require('bedrock');
+
+const schema = {
+  required: true,
+  title: 'Decentralized Identifier',
+  description: 'A decentralized identifier.',
+  type: 'string',
+  // FIXME: review regex pattern for spec conformance`
+  pattern: "^(did:v1:|did:)([A-Za-z0-9.-]+)$",
+  errors: {
+    invalid: 'The decentralized identifier is invalid.',
+    missing: 'Please enter a decentralized identifier.'
+  }
+};
+
+module.exports = extend => {
+  if(extend) {
+    return bedrock.util.extend(true, bedrock.util.clone(schema), extend);
+  }
+  return schema;
+};
---
+++ schemas/veres-one-validator.js
@@ -0,0 +1,130 @@
+/*!
+ * Copyright (c) 2017 Digital Bazaar, Inc. All rights reserved.
+ */
+const constants = require('bedrock').config.constants;
+const schemas = require('bedrock-validation').schemas;
+const did = require('./did');
+
+const config = {
+  title: 'Veres One Validator Config',
+  required: true,
+  type: 'object',
+  properties: {
+    type: {
+      type: 'string',
+      enum: ['VeresOneValidator2017'],
+      required: true
+    },
+    eventFilter: {
+      title: 'Event Filter',
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          type: {
+            type: 'string',
+            required: true
+          },
+          eventType: {
+            type: 'array',
+            items: {
+              type: 'string'
+            },
+            required: true
+          }
+        },
+        additionalProperties: false
+      },
+      required: true
+    }
+  },
+  additionalProperties: false
+};
+
+const capability = {
+  title: 'Capability',
+  type: 'object',
+  required: true,
+  properties: {
+    permission: {
+      type: 'string',
+      required: true
+    },
+    entity: did(),
+    permittedProofType: {
+      title: 'Permitted Proof Type',
+      type: 'array',
+      minItems: 1,
+      items: [{
+        type: 'object',
+        properties: {
+          proofType: {
+            type: 'string',
+            enum: ['LinkedDataSignature2015'],
+            required: true
+          },
+          additionalProperties: false
+        },
+        additionalProperties: false
+      }, {
+        type: 'object',
+        properties: {
+          proofType: {
+            type: 'string',
+            enum: ['EquihashProof2017'],
+            required: true
+          },
+          equihashParameterAlgorithm: {
+            type: 'string',
+            required: true,
+            enum: ['VeresOne2017']
+          }
+        },
+        additionalProperties: false
+      }],
+      required: true
+    }
+  },
+  additionalProperties: false
+};
+
+const didDocument = {
+  title: 'DID Document',
+  required: true,
+  type: 'object',
+  properties: {
+    '@context': schemas.jsonldContext(constants.VERES_ONE_CONTEXT_V1_URL),
+    id: did(),
+    authorizationCapability: {
+      type: 'array',
+      required: true,
+      items: capability
+    },
+    authenticationCredential: {
+      type: 'array',
+      minItems: 1,
+      items: {
+        type: 'object',
+        properties: {
+          id: {
+            type: 'string',
+            required: true
+          },
+          type: {
+            type: 'string',
+            required: true,
+            enum: ['CryptographicKey'],
+          },
+          owner: did(),
+          publicKeyPem: schemas.publicKeyPem()
+        },
+        additionalProperties: false
+      },
+      required: true
+    },
+  },
+  additionalProperties: false
+};
+
+module.exports.config = () => config;
+module.exports.didDocument = () => didDocument;
