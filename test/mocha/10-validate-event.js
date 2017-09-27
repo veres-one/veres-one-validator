7a29eb59a279d988249686b511fc0766af355694
Matthew Collier <collier.matthew@gmail.com>
2017-09-27 12:01:57 -0400

    Fix missing return.

--- test/mocha/10-validate-event.js
+++ test/mocha/10-validate-event.js
@@ -46,6 +46,107 @@ describe('validateEvent API', () => {
         done();
       });
     });
+    it('validation fails if Equihash signature is missing', done => {
+      async.auto({
+        sign: callback => signDocument({
+          creator: mockData.authorizedSigners.alpha,
+          privateKeyPem: mockData.keys.alpha.privateKey,
+          doc: mockData.events.alpha
+        }, callback),
+        check: ['sign', (results, callback) => {
+          const signedDoc = bedrock.util.clone(mockData.events.alpha);
+          signedDoc.signature = [
+            results.sign.signature
+          ];
+          voValidator.validateEvent(
+            signedDoc,
+            mockData.ledgers.alpha.config.ledgerConfiguration.eventValidator[0],
+            err => {
+              should.exist(err);
+              err.name.should.equal('ValidationError');
+              should.exist(err.details.updateSignatureType);
+              err.details.updateSignatureType.should.be.an('array');
+              err.details.updateSignatureType.should.have.same.members([
+                'LinkedDataSignature2015', 'EquihashProof2017']);
+              callback();
+            });
+        }]
+      }, err => {
+        assertNoError(err);
+        done();
+      });
+    });
+    it('validation fails if jsonld-signature is missing', done => {
+      async.auto({
+        proof: callback => equihashSigs.sign({
+          doc: mockData.events.alpha,
+          n: cfg.equihash.equihashParameterN,
+          k: cfg.equihash.equihashParameterK
+        }, callback),
+        check: ['proof', (results, callback) => {
+          const signedDoc = bedrock.util.clone(mockData.events.alpha);
+          signedDoc.signature = [
+            results.proof.signature
+          ];
+          voValidator.validateEvent(
+            signedDoc,
+            mockData.ledgers.alpha.config.ledgerConfiguration.eventValidator[0],
+            err => {
+              should.exist(err);
+              err.name.should.equal('ValidationError');
+              should.exist(err.details.updateSignatureType);
+              err.details.updateSignatureType.should.be.an('array');
+              err.details.updateSignatureType.should.have.same.members([
+                'LinkedDataSignature2015', 'EquihashProof2017']);
+              callback();
+            });
+        }]
+      }, err => {
+        assertNoError(err);
+        done();
+      });
+    });
+    it('validation fails if the ld-signature is not by the owner', done => {
+      async.auto({
+        proof: callback => equihashSigs.sign({
+          doc: mockData.events.alpha,
+          n: cfg.equihash.equihashParameterN,
+          k: cfg.equihash.equihashParameterK
+        }, callback),
+        sign: callback => signDocument({
+          creator: mockData.authorizedSigners.alpha,
+          // NOTE: the wrong private key is used here to sign the document
+          privateKeyPem: mockData.keys.beta.privateKey,
+          doc: mockData.events.alpha
+        }, callback),
+        check: ['proof', 'sign', (results, callback) => {
+          const signedDoc = bedrock.util.clone(mockData.events.alpha);
+          signedDoc.signature = [
+            results.sign.signature,
+            results.proof.signature
+          ];
+          voValidator.validateEvent(
+            signedDoc,
+            mockData.ledgers.alpha.config.ledgerConfiguration.eventValidator[0],
+            err => {
+              should.exist(err);
+              err.name.should.equal('ValidationError');
+              should.exist(err.details.keyResults);
+              const keyResults = err.details.keyResults;
+              keyResults.should.be.an('array');
+              keyResults.should.have.length(1);
+              keyResults[0].should.be.an('object');
+              keyResults[0].verified.should.be.false;
+              keyResults[0].publicKey.should.equal(
+                mockData.authorizedSigners.alpha);
+              callback();
+            });
+        }]
+      }, err => {
+        assertNoError(err);
+        done();
+      });
+    });
   }); // end WebLedgerEvent
 
   describe.skip('WebLedgerConfigurationEvent', () => {
