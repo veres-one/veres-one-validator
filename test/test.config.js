e3afa53bf71c301f6678cff05658e9946ad8470b
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-10-31 18:14:37 -0400

    Add comments to test and default environment config.

--- test/test.config.js
+++ test/test.config.js
@@ -9,6 +9,9 @@ config.mocha.tests.push(path.join(__dirname, 'mocha'));
 
 const cfg = config['veres-one-validator'];
 
+// Set mode to 'test', so that DIDs are created as 'did:v1:test:...' in tests
+cfg.environment = 'test';
+
 // use quick equihash setting for tests
 cfg.equihash.equihashParameterN = 64;
 cfg.equihash.equihashParameterK = 3;
