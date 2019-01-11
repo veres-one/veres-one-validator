0d7217936af31e323fea0302cdc8fddfcb247149
Matthew Collier <collier.matthew@gmail.com>
2019-01-11 13:12:19 -0500

    Validate basisBlockHeight param.

--- test/mocha/05-validate-async.js
+++ test/mocha/05-validate-async.js
@@ -20,8 +20,6 @@ describe.only('validate API', () => {
     const {CapabilityInvocation} = require('ocapld');
     const {Ed25519Signature2018} = jsigs.suites;
     const capabilityAction = 'RegisterDid';
-    // FIXME: this step can be removed after crypto-ld creates default id
-    // based on controller
     const did = `did:v1:test:nym:${keyFingerprint}`;
     // cryptonym dids are based on fingerprint of capabilityInvokation key
     mockDoc.id = did;
@@ -48,6 +46,7 @@ describe.only('validate API', () => {
       purpose: new CapabilityInvocation({capability, capabilityAction})
     });
     const result = await voValidator.validate({
+      basisBlockHeight: 0,
       ledgerNode: mockData.ledgerNode,
       validatorInput: s,
       validatorConfig: mockData.ledgerConfigurations.alpha
@@ -56,6 +55,51 @@ describe.only('validate API', () => {
     should.exist(result);
     result.valid.should.be.a('boolean');
     result.valid.should.be.true;
+  });
+  it('validates a proper UpdateWebLedgerRecord operation', async () => {
+    const mockOperation = bedrock.util.clone(mockData.operations.create);
+    const mockDoc = bedrock.util.clone(mockData.privateDidDocuments.alpha);
+    const key = await Ed25519KeyPair.generate();
+    const keyFingerprint = `z${key.fingerprint()}`;
+    const {CapabilityInvocation} = require('ocapld');
+    const {Ed25519Signature2018} = jsigs.suites;
+    const capabilityAction = 'RegisterDid';
+    const did = `did:v1:test:nym:${keyFingerprint}`;
+    // cryptonym dids are based on fingerprint of capabilityInvokation key
+    mockDoc.id = did;
+    key.id = generateKeyId({did, key});
+    const controller = did;
+    key.controller = controller;
+    const capability = controller;
+    mockDoc.capabilityInvocation[0] = {
+      id: key.id,
+      type: key.type,
+      controller: key.controller,
+      publicKeyBase58: key.publicKeyBase58
+    };
+    // add the new document to the mock document loader as if it were on ledger
+    // must clone this going into the document loader, otherwise it will be
+    // mutated
+    mockData.existingDids[did] = bedrock.util.clone(mockDoc);
+    mockOperation.record = mockDoc;
+    // add an AuthorizeRequest proof that will pass json-schema validation for
+    // testnet v2 *not* a valid signature
+    mockOperation.proof = bedrock.util.clone(mockData.proof);
+    const s = await jsigs.sign(mockOperation, {
+      documentLoader,
+      suite: new Ed25519Signature2018({compactProof: false, key}),
+      purpose: new CapabilityInvocation({capability, capabilityAction})
+    });
+    const result = await voValidator.validate({
+      basisBlockHeight: 10,
+      ledgerNode: mockData.ledgerNode,
+      validatorInput: s,
+      validatorConfig: mockData.ledgerConfigurations.alpha
+        .operationValidator[0],
+    });
+    should.exist(result);
+    result.valid.should.be.a('boolean');
+    result.valid.should.be.false;
   });
 });
 
