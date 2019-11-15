350fb2f53be3642252cc69211cf9fdfb87843b20
Matthew Collier <mcollier@digitalbazaar.com>
2019-11-15 18:16:46 -0500

    Add tests.

--- test/mocha/12-validate-ledger-configuration.js
+++ test/mocha/12-validate-ledger-configuration.js
@@ -37,6 +37,62 @@ describe('validate API WebLedgerConfiguration', () => {
     result.valid.should.be.a('boolean');
     result.valid.should.be.true;
   });
+  it('rejects a configuration w/missing electorSelectionMethod', async () => {
+    const ledgerConfiguration = clone(mockData.ledgerConfigurations.alpha);
+
+    delete ledgerConfiguration.electorSelectionMethod;
+
+    // The public key material is derived from the nym DID because the
+    // maintainers DID does not yet exist on the ledger
+    const maintainerDoc = await v1.generate();
+    const method = maintainerDoc.getVerificationMethod(
+      {proofPurpose: 'capabilityInvocation'});
+    const signingKey = maintainerDoc.keys[method.id];
+    const s = await jsigs.sign(ledgerConfiguration, {
+      compactProof: false,
+      documentLoader,
+      suite: new Ed25519Signature2018({key: signingKey}),
+      purpose: new AssertionProofPurpose()
+    });
+
+    const result = await voValidator.validate({
+      ledgerNode: mockData.ledgerNode,
+      validatorInput: s,
+    });
+    should.exist(result.valid);
+    result.valid.should.be.a('boolean');
+    result.valid.should.be.false;
+    should.exist(result.error);
+    result.error.name.should.equal('ValidationError');
+  });
+  it('rejects a configuration w/missing sequence', async () => {
+    const ledgerConfiguration = clone(mockData.ledgerConfigurations.alpha);
+
+    delete ledgerConfiguration.sequence;
+
+    // The public key material is derived from the nym DID because the
+    // maintainers DID does not yet exist on the ledger
+    const maintainerDoc = await v1.generate();
+    const method = maintainerDoc.getVerificationMethod(
+      {proofPurpose: 'capabilityInvocation'});
+    const signingKey = maintainerDoc.keys[method.id];
+    const s = await jsigs.sign(ledgerConfiguration, {
+      compactProof: false,
+      documentLoader,
+      suite: new Ed25519Signature2018({key: signingKey}),
+      purpose: new AssertionProofPurpose()
+    });
+
+    const result = await voValidator.validate({
+      ledgerNode: mockData.ledgerNode,
+      validatorInput: s,
+    });
+    should.exist(result.valid);
+    result.valid.should.be.a('boolean');
+    result.valid.should.be.false;
+    should.exist(result.error);
+    result.error.name.should.equal('ValidationError');
+  });
   it('rejects a ledger configuration with an invalid signature', async () => {
     const ledgerConfiguration = clone(mockData.ledgerConfigurations.alpha);
 
@@ -67,7 +123,7 @@ describe('validate API WebLedgerConfiguration', () => {
     should.exist(result.error);
     result.error.message.should.contain('Proof verification failed.');
   });
-  it('rejects a ledger configuration with an invalid nym did', async () => {
+  it('rejects configuration signed with an invalid nym did', async () => {
     const ledgerConfiguration = clone(mockData.ledgerConfigurations.alpha);
 
     // The public key material is derived from the nym DID because the
@@ -87,6 +143,7 @@ describe('validate API WebLedgerConfiguration', () => {
     s.proof.verificationMethod =
       'did:v1:nym:z6MksN1qAquFzdSgXwVTAYuuLGfckt3UpkhHwFwPsbSiwrUB#' +
       // the leading `z` has been removed here to break the encoding
+      // 6 is not a valid encoding prefix
       '6MksN1qAquFzdSgXwVTAYuuLGfckt3UpkhHwFwPsbSiwrUB';
 
     const result = await voValidator.validate({
@@ -100,5 +157,40 @@ describe('validate API WebLedgerConfiguration', () => {
     result.error.message.should.contain(
       'The signing key is not properly encoded.');
     result.error.cause.message.should.contain('Unsupported encoding');
+  });
+  it('rejects configuration signed with an invalid nym did', async () => {
+    const ledgerConfiguration = clone(mockData.ledgerConfigurations.alpha);
+
+    // The public key material is derived from the nym DID because the
+    // maintainers DID does not yet exist on the ledger
+    const maintainerDoc = await v1.generate();
+    const method = maintainerDoc.getVerificationMethod(
+      {proofPurpose: 'capabilityInvocation'});
+    const signingKey = maintainerDoc.keys[method.id];
+
+    const s = await jsigs.sign(ledgerConfiguration, {
+      compactProof: false,
+      documentLoader,
+      suite: new Ed25519Signature2018({key: signingKey}),
+      purpose: new AssertionProofPurpose()
+    });
+
+    s.proof.verificationMethod =
+      'did:v1:nym:z6MksN1qAquFzdSgXwVTAYuuLGfckt3UpkhHwFwPsbSiwrUB#' +
+      // the 9 here specifies a decimal encoding
+      '91234';
+
+    const result = await voValidator.validate({
+      ledgerNode: mockData.ledgerNode,
+      validatorInput: s,
+    });
+    should.exist(result.valid);
+    result.valid.should.be.a('boolean');
+    result.valid.should.be.false;
+    should.exist(result.error);
+    result.error.message.should.contain(
+      'The signing key is not properly encoded.');
+    result.error.cause.message.should.equal(
+      'Key ID is not a multiformats encoded ed25519 public key.');
   });
 });
