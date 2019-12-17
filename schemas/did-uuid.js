52d1fb98099ea160f31570273dfb34d65c3bf26d
Matthew Collier <mcollier@digitalbazaar.com>
2019-12-17 12:28:21 -0500

    Validate URN DIDs with test prefix.

--- schemas/did-uuid.js
+++ schemas/did-uuid.js
@@ -3,8 +3,15 @@
  */
 'use strict';
 
+const bedrock = require('bedrock');
+const {config} = bedrock;
+require('../lib/config');
+
+const cfg = config['veres-one-validator'];
+
+const prefix = cfg.environment === 'test' ? 'did:v1:test' : 'did:v1';
 /* eslint-disable-next-line max-len */
-const pattern = '^did:v1:uuid:([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})$';
+const pattern = `^${prefix}:uuid:([a-fA-F0-9]{8}-[a-fA-F0-9]{4}-4[a-fA-F0-9]{3}-[89abAB][a-fA-F0-9]{3}-[a-fA-F0-9]{12})$`;
 
 const schema = {
   title: 'DID UUID',
